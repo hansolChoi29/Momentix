7ba1505e9f011e43929547e18d474d674569c8d6
hansolChoi29 <ggksthf29@gmail.com>
2026-03-26 21:21:57 +0900

    chore: 줄 예쁘게

--- src/test/java/com/example/momentix/domain/common/util/AgeUtilTest.java
+++ src/test/java/com/example/momentix/domain/common/util/AgeUtilTest.java
@@ -9,7 +9,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AgeUtilTest {
-
     @Test
     @DisplayName("생일_null이면_false_반환")
     void isOverAge_nullBirthDate_returnsFalse() {
